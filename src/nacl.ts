aaa8536ac295aba06d77a1fc4d2156e734e02ad0
Kayo Phoenix <kayo@illumium.org>
2018-04-18 09:24:12 +0500

    Added packaging

---
+++ src/nacl.ts
@@ -0,0 +1,22 @@
+// Ported in 2014 by Dmitry Chestnykh and Devi Mandiri.
+// Public domain.
+//
+// Implementation derived from TweetNaCl version 20140427.
+// See for details: http://tweetnacl.cr.yp.to/
+//
+// Ported to TypeScript in 2018 by K. <kayo@illumium.org>
+//
+
+export * from './array';
+export * from './convert';
+export * from './verify';
+export * from './random';
+export * from './scalarmult';
+export * from './secretbox';
+export * from './box';
+export * from './sign';
+export * from './hash';
+export * from './auth';
+export * from './blake2s';
+export * from './blake2b';
+export * from './sealedbox';
