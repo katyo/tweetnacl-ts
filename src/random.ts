bf2aaab93ae13ea0b3784a20a5ff11c2e6abd9ca
Joseph Innes <joseph.innes@swissinfo.ch>
2020-05-23 18:02:56 +0200

    Clean up random.ts

--- src/random.ts
+++ src/random.ts
@@ -1,10 +1,7 @@
 import { ByteArray } from './array.ts';
-import { _randomBytes } from './server/random.ts';
-
-export { _randomBytes };
 
 export function randomBytes(n: number): ByteArray {
-    const b = ByteArray(n);
-    _randomBytes(b, n);
+    let b = ByteArray(n);
+    window.crypto.getRandomValues(b);
     return b;
 }
--- src/server/random.ts
+++
@@ -1,7 +0,0 @@
-import { ByteArray } from '../array.ts';
-
-export function _randomBytes(x: ByteArray, n: number) {
-    for (let i = 0; i < n; i++) {
-        x[i] = Math.floor(Math.random() * Math.floor(255));
-    }
-}
